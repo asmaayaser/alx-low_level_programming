195901e945eca97b6deb9d9f1422e3e1e9e86bda
asmaa <yaserasmaa358.com>
2023-03-17 17:34:40 -0700

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
