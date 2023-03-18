acba4dfdf71083ba3ec4fbb16130c1816bae4d41
asmaa <yaserasmaa358.com>
2023-03-17 17:58:53 -0700

    g

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n = 122;
+while (n <= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
