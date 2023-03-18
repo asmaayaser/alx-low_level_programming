a4ddf3783e277f215f62f1e98aea85ad5329d150
asmaa <yaserasmaa358.com>
2023-03-17 17:55:20 -0700

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+for (n = 0 ; n <= 9 ; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
+}
