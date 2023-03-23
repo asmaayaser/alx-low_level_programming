0b444a9bbf37bdb45bea3a16c8a3a9c531331bd2
asmaa <yaserasmaa358.com>
2023-03-22 19:24:17 -0700

    h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - print letters in lowercase *10
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 1 ; j <= 10 ; j++)
+{
+for (i = 97 ;  i <= 122 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
+
