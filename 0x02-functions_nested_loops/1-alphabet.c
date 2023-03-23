ac578f3658a75c79b5e138851ca3fb10b7428692
asmaa <yaserasmaa358.com>
2023-03-22 19:12:21 -0700

    h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet in lower case
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 97 ; i <= 120 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
