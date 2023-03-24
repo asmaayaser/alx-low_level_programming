4166813be0658ba20cd36dd1bb918c74de5d16e6
asmaa <yaserasmaa358.com>
2023-03-23 17:48:51 -0700

    f

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
 *isupper -uppercase letters
+@c character
 * Return: return 0 or 1
 */
 int _isupper(int c)
 {
-if (c >= 'A' && C <= 'Z')
+if (c >= 65 && C <= 90)
 {
 return (1);
 }
